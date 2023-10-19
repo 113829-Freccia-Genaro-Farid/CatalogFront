29810d2133e35748571381d3c9cdc80943255f96
113829-Freccia-Genaro-Farid <113829@tecnicatura.frc.utn.edu.ar>
2023-10-19 20:08:43 -0300

    -.-

---
+++ src/app/modules/catalog/components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fn-home-catalog',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  registrar = false;
+  modificar=true;
+
+
+  mostrarRegistrar(){
+    this.registrar=true;
+    this.modificar=false;
+  }
+  mostrarUpdate(){
+    this.registrar=false;
+    this.modificar=true;
+  }
+}
